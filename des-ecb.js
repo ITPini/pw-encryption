91c34246caea15f855d3930a57ba5242732088d5
ITPini <43484432+ITPini@users.noreply.github.com>
2022-03-10 23:54:30 +0100

    Update des-ecb.js

--- des-ecb.js
+++ des-ecb.js
@@ -1,10 +1,5 @@
 const crypto = require("crypto");
 
-// Documentation: https://nodejs.org/dist/latest-v10.x/docs/api/crypto.html#crypto_class_cipher
-// This should not be used for production, since decryption is a two-way function.
-// Encryption ≠ Hashing
-// Salted password is composed of "algorithm$salt$password"
-
 const algorithm = "des-ecb"
 const secretKey = "D0E276D0144890D3";                   // 16 bytes
 const salt = "DYOycGhXqyfO";                            // can be any salted string
